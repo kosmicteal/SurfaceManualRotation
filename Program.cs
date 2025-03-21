8c6538bbda1d471ce4538ee056b1f370ace99d16
Kosmic Teal <69432458+kosmicteal@users.noreply.github.com>
2025-03-21 17:31:56 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32;
+
+namespace SurfaceManualRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\PriorityControl", true))
+            {
+                if (registryKey == null)
+                    return;
+                string str = registryKey.GetValue("ConvertibleSlateMode").ToString();
+                registryKey.SetValue("ConvertibleSlateMode", str == "0" ? "1" : "0", RegistryValueKind.DWord);
+                registryKey.Close();
+            }
+        }
+    }
+}
